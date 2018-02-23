36da9623b36a0c8bc060010681a4b5f729b02058
Nick Terry <nick.terry1@outlook.com>
2018-02-23 15:49:07 -0500

    second commit

---
+++ exercises/project1/project1/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace project1
+{
+    static void Main(string[] args)
+    {
+        public void circumArea() // First part calculates circumference and second part calculates area
+        {
+            float circum;
+            float pi = 3.14159F;
+            Console.WriteLine("enter a value for the radius");
+            float r = float.Parse(Console.ReadLine());
+            circum = 2 * pi* r;
+            Console.WriteLine($"The circumference of the circle is {circum}");
+
+            float area = pi * (r * r);
+            Console.WriteLine($"The area of the circle is {area}");
+        
+        }
+
+        public void calVolume(string[] args)
+            {
+                float vol;
+                float pi = 3.14159F;
+                Console.WriteLine("Please enter a radius");
+                float r = float.Parse(Console.ReadLine());
+                vol = ((4 / 3) * pi * (r * r * r)) / 2;
+                Console.WriteLine(vol);
+            }
+        }
+    }
+}
+
---
+++ exercises/project1_triArea/TriArea/TriArea/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace TriArea          // calculates the area of a triangle of sides a,b, and c
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Enter a value for the first side");                             // The first side of the triangle is of length "a"
+            int sideA = int.Parse(Console.ReadLine());                                         // Reads the user input for side 'a'
+            Console.WriteLine("Enter a value for the second side");                            // The second side of the triangle is of lenth 'b'
+            int sideB = int.Parse(Console.ReadLine());                                         // Reads the user input for side 'b'
+            Console.WriteLine("Enter a value for the third side");                             // The third side of the triangle is of lenth 'c'
+            int sideC = int.Parse(Console.ReadLine());                                         // Reads the user input for the side 'c'
+
+            double p = (sideA + sideB + sideC) / 2;                                            // p is the sum of all sides divided by two. This variable
+                                                                                               // will be used for the final area equation.
+            double areaFinal = Math.Sqrt(p * (p - sideA) * (p - sideB) * (p - sideB));         // Herons formula
+            Console.WriteLine($"The area of the tringle is {areaFinal}");                      // Displays the results from Herons formula
+
+            
+
+        }
+    }
+}
