a927b17a0f4ab18c89928bf001f2f40faf832c6d
Nick Terry <nick.terry1@outlook.com>
2018-03-24 15:35:59 -0400

    project dia & labs

---
+++ labs/StructsAndEnums.cs
@@ -0,0 +1,50 @@
+﻿#region Using directives
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+#endregion
+
+namespace StructsAndEnums
+{
+    class Program
+    {
+        static void doWork()
+        {
+            Month first = Month.December;
+            Console.WriteLine(first);
+            Console.WriteLine((int)first);
+
+            first++;
+            Console.WriteLine(first);
+
+            Date defaultDate = new Date();
+            Console.WriteLine(defaultDate);
+
+            Date WeddingAnniversary = new Date(2015, Month.July, 4);
+            Console.WriteLine(WeddingAnniversary);
+
+            Date weddingAnniversaryCopy = WeddingAnniversary;
+            Console.WriteLine($"Value of copy is {weddingAnniversaryCopy}");
+
+            WeddingAnniversary.AdvanceMonth();
+            Console.WriteLine($"New value of weddingAnniversary is {WeddingAnniversary}");
+            Console.WriteLine($"Value of copy is still {weddingAnniversaryCopy}");
+        }
+
+        static void Main()
+        {
+            try
+            {
+                doWork();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+        }
+    }
+}
