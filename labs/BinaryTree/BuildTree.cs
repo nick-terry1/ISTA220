7fb3797f40cd0727c6d93451a4cb7aa2f2fefbaa
Nick Terry <nick.terry1@outlook.com>
2018-04-17 10:49:57 -0400

    commit labs ch 17 & ch 18

---
+++ labs/BinaryTree/BuildTree.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using BinaryTree;
+
+namespace BuildTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tree<char> charTree = null;
+            InsertIntoTree<char>(ref charTree, 'm', 'x', 'a', 'm', 'z', 'z', 'n', 'x', 'z', 'z');
+            string sortedData = charTree.WalkTree();
+            Console.WriteLine($"Sorted data is : {sortedData}");
+        }
+
+        public static void InsertIntoTree<TItem>(ref Tree<TItem> tree, params TItem[] data) where TItem : IComparable<TItem>
+        {
+            foreach (TItem datum in data)
+            {
+                if (tree == null)
+                    tree = new Tree<TItem>(datum);
+                else
+                    tree.Insert(datum);
+            }
+        }
+    }
+}
